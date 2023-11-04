f9cc2f631808c835e03a3a54a099ff30bfc20740
Sparsh101AI <Sparshpatel6165@gmail.com>
2023-11-04 17:47:57 -0400

    Improved parser

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
 import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+
 function App() {
-  const [speech, setSpeech] = useState("");
+  const [speech, setSpeech] = useState('');
 
   const {
     error,
@@ -17,23 +18,24 @@ function App() {
     stopSpeechToText,
   } = useSpeechToText({
     continuous: true,
-    useLegacyResults: false
+    useLegacyResults: false,
   });
 
   const [fact, setFact] = useState('');
 
   const fetchFact = async () => {
     try {
+      let prompt = 'Rewrite the following into shorter and concise sentences: ' + speech;
       const response = await axios.post(
         'https://api.openai.com/v1/engines/text-davinci-002/completions',
         {
-          prompt: 'Convert the following notes into fact ',
-          max_tokens: 50
+          prompt: prompt,
+          max_tokens: 50,
         },
         {
           headers: {
-            'Authorization': `Bearer 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`
-          }
+            'Authorization': `Bearer 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`,
+          },
         }
       );
 
@@ -45,10 +47,10 @@ function App() {
   };
 
   useEffect(() => {
-    let totalSpeech = "";
+    let totalSpeech = '';
     console.log(results, interimResult);
-    results.forEach(text => {
-      if (text.transcript) totalSpeech += text.transcript + ". ";
+    results.forEach((text) => {
+      if (text.transcript) totalSpeech += text.transcript + '. ';
     });
     if (interimResult) totalSpeech += interimResult;
     setSpeech(totalSpeech);
@@ -56,39 +58,27 @@ function App() {
 
   if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
 
-
   return (
-      <div className="main-container">
-        <div className="title-wrapper">
+    <div className="main-container">
+      <div className="title-wrapper">
         <span>VoiceNotes</span>
-        </div>
-        <div className="speech-content">
-          <p>{speech}</p>
-        </div>
-        <div className='btn-container'>
-          <span className="btn" onClick={isRecording ? stopSpeechToText : startSpeechToText}>
-            {isRecording ? <MicIcon style={{color: 'rgba(0,0,0,0.8)', fontSize: 30}}/> : <MicOffIcon style={{color: 'rgba(0,0,0,0.8)', fontSize: 30}}/>}
-          </span>
-          <span className="btn">
-            <DocumentScannerIcon fontSize="medium" style={{color: 'rgba(0,0,0,0.8)', fontSize: 25}} onClick={fetchFact}>Press me for notes</DocumentScannerIcon>
-          </span>
-        </div>
-        <p>{fact}</p>
       </div>
+      <div className="speech-content">
+        <p>{speech}</p>
+      </div>
+      <div className="btn-container">
+        <span className="btn" onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+          {isRecording ? <MicIcon style={{ color: 'rgba(0,0,0,0.8)', fontSize: 30 }} /> : <MicOffIcon style={{ color: 'rgba(0,0,0,0.8)', fontSize: 30 }} />}
+        </span>
+        <span className="btn">
+          <DocumentScannerIcon fontSize="medium" style={{ color: 'rgba(0,0,0,0.8)', fontSize: 25 }} onClick={fetchFact}>
+            Press me for notes
+          </DocumentScannerIcon>
+        </span>
+      </div>
+      <p>{fact}</p>
+    </div>
   );
 }
-
-    // <div>
-    //   <h1>Recording: {isRecording.toString()}</h1>
-    //   <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
-    //     {isRecording ? 'Stop Recording' : 'Start Recording'}
-    //   </button>
-    //   <ul>
-    //     {results.map((result) => (
-    //       <li key={result.timestamp}>{result.transcript}</li>
-    //     ))}
-    //     {interimResult && <li>{interimResult}</li>}
-    //   </ul>
-    // </div>
 
 export default App;
